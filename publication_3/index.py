d9111b12c9e8e1bd8b279aa20b1010edd3230d1d
Doodle <doodle@example.com>
2024-11-17 23:20:52 +0300

    Описан раздел Представление решения

--- publication_3/index.py
+++ publication_3/index.py
@@ -14,7 +14,7 @@ for i in range(m * k):
 	X_bin[i] = i + 1
 
 for i in range(m):
-	X_dis[i] = randint(0, k)
+	X_dis[i] = randint(1, k)
 
 print('X_bin', X_bin)
 print('X_dis', X_dis)
@@ -23,9 +23,18 @@ def get_bin(i, j):
 	index = i * k - (k - j)
 	return X_bin[index - 1]
 
+def get_dis(i, j):
+	index = i
+	return X_dis[index - 1]
+
 for i in range(m):
 	for j in range(k):
 		A_bin[i][j] = get_bin(i + 1, j + 1)
+		dis_index = get_dis(i + 1, j + 1)
+		A_dis[i][dis_index - 1] = 1 
 
 print('A_bin')
 print(A_bin)
+
+print('A_dis')
+print(A_dis)
