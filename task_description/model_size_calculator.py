c643abb5505e5bb74188d3ae3502710a29259fc5
dambr <dambr@localhost>
2024-12-21 16:03:31 +0300

    Построены сглаживающие графики

---
+++ task_description/model_size_calculator.py
@@ -0,0 +1,22 @@
+class ModelSizeCalculator:
+    def __init__(self, k, l, m, n):
+        self.k = k
+        self.l = l
+        self.m = m
+        self.n = n
+
+    def calculate_variables_count(self):
+        k = self.k
+        l = self.l
+        m = self.m
+        n = self.n
+
+        return m * k + l * (k * (1 + m) + 2 * n)
+
+    def calculate_constraints_count(self):
+        k = self.k
+        l = self.l
+        m = self.m
+        n = self.n
+
+        return l * (m + 3 * m * k + 2 * k + 5 * n)
