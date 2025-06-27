1c7b380dbcf935c3175b4d8710e215129dbcda3c
Doodle <doodle@example.com>
2025-06-27 15:25:26 +0300

    Начало программной реализации

---
+++ publication_4/script/df_tokenizer.py
@@ -0,0 +1,11 @@
+class DfTokenizer():
+
+    def __init__(self, df, value_column):
+        self.df = df
+        self.value_column = value_column
+
+    def get_token(self, value):
+        pass
+
+    def get_value(self, token):
+        pass
---
+++ publication_4/script/requirements_tokenizer.py
@@ -0,0 +1,10 @@
+class RequirementsTokenizer():
+    
+    def __init__(self, requirements_df):
+        self.requirements_df = requirements_df
+
+    def get_token(self, id):
+        pass
+
+    def get_id(self, token):
+        pass
